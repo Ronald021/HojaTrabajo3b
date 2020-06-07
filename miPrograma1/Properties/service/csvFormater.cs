0588167c790fbf94d11acf75da4cd3c510cc25de
Ronald <ottoniel.21@outlook.es>
2020-06-07 17:37:53 -0600

    Versión inicial de la Hoja de trabajo 3

---
+++ miPrograma1/Properties/service/csvFormater.cs
@@ -0,0 +1,35 @@
+﻿﻿using System;
+using System.IO;
+
+namespace miPrograma1.Properties.service
+{
+    public class csvFormater : formatDecorator
+    {
+        public csvFormater()
+        {
+            
+        }
+        public csvFormater(format fDecorator) : base(fDecorator)
+        {
+        }
+
+        public override void create(string[] archivo)
+        {
+            System.Console.WriteLine("CSV");
+            float tamaño = archivo.Length;
+            String lines = "";
+            string docPath = Directory.GetCurrentDirectory();;
+            File.WriteAllText(Path.Combine(docPath, "csv.csv"), "");
+            for (int i = 0; i < tamaño-1; i++)
+            {
+                string text = System.IO.File.ReadAllText(@archivo[i]);
+                text= text.Replace(" ", ",");
+                System.Console.WriteLine("Archivo No. : " +(i+1)+"\n"+text + "\n");
+                lines = "Archivo No. : " + (i + 1) + "\n" + text + "\n";
+                File.AppendAllText(Path.Combine(docPath, "csv.csv"),lines );
+                
+            }
+        }
+        
+    }
+}
---
+++ miPrograma1/Properties/view/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using miPrograma1.Properties.service;
+
+namespace miPrograma1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String form = args[(args.Length - 1)];
+            formatMaker hi = new formatMaker();
+            hi.convert(args,form);
+        }
+    }
+}
